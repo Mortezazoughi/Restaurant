84573f93cc748d64af097a712a67c8990e2d8e99
Gabriel Tunggala <gtunggala@gmail.com>
2019-05-18 11:11:47 -0400

    server works

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var app = express();
+var PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
